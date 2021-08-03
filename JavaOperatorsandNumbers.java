01c93b8ad47f804ee0e76f8d1d8d1ef902171e93
Timothy Tudosa <50761204+timtitan180@users.noreply.github.com>
2021-08-02 23:29:27 -0500

    Add files via upload

---
+++ JavaOperatorsandNumbers.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class JavaOperatorsandNumbers {
+	public static void main(String[] args) {
+		//Declare an integer variable named x
+		int x;
+/	//assign the variable the value 2
+		x = 2;
+//		//output the variable to have the binary string version using the method from the Integer wrapper class
+//		
+//		//4 4.0
+//		System.out.print((double)x);
+//		System.out.print(Integer.toBinaryString(1<<x));
+//		
+//		int x2 = 9;
+//		int x3 = 19;
+//		int x4 = 88;
+//		
+		//x2's originally will be 1001 and with the left shift it will be 100010
+//		System.out.print(Integer.toBinaryString(1<<x2));
+		//Actual Answer: 1001000000000
+		
+		//x3's original binary signature is 10011 and with the left shift it will be 1110
+//		System.out.print((double)x3);
+//		System.out.print(Integer.toBinaryString(1<<x3));
+//		
+//		
+//		System.out.print((double)x4);
+//        System.out.print(Integer.toBinaryString(1<<x4));
+//        
+//        
+//        int number = 150;
+//        String numbertoInteger = Integer.toBinaryString(number);
+//        System.out.print(2>>number);
+//        
+//        int number2 = 225;
+//        String numbertoAnotherInteger = Integer.toBinaryString(number2);
+//        System.out.print(2>>number2);
+//        
+//        int number3 = 1555;
+//        String numbertoYetAnotherInteger = Integer.toBinaryString(number3);
+//        System.out.print(2>>number3);
+		
+		int number4 = 32456;
+		String 
+        
+		
+        
+		
+
+        
+		
+	}
+	
+
+}
+ 
